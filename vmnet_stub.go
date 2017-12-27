f202a0dc1f00dc4abce7edaa547df9f60ba80a0c
NobiDev <nobi@nobidev.com>
2017-12-27 13:00:07 -0800

    Move GetNetAddr.
    The build constraints in vmnet.go cause issues.

--- vmnet_stub.go
+++ vmnet_stub.go
@@ -18,7 +18,9 @@ limitations under the License.
 
 package nobidev
 
-import "errors"
+import (
+	"errors"
+)
 
 func GetMACAddressFromUUID(UUID string) (string, error) {
 	return "", errors.New("Function not supported on CGO_ENABLED=0 binaries")
